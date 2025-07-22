b57b5423e86a36c4d0cca82f2850e8214e6023cf
SAIKO9X <cardosed3@gmail.com>
2025-07-22 13:26:00 -0300

    fix(core): Garante a ordem de execução dos semeadores de dados
    Utiliza a anotação @Order para forçar o CategorySeeder (@Order(1))
    a rodar antes do DataSeeder (@Order(2)).
    
    Isso resolve o problema onde os produtos eram criados antes das
    categorias existirem, garantindo que a inicialização da aplicação
    ocorra sem erros de 'Categoria não encontrada'.

--- back/src/main/java/com/ecommerce/CategorySeeder.java
+++ back/src/main/java/com/ecommerce/CategorySeeder.java
@@ -4,10 +4,12 @@ import com.ecommerce.model.entities.Category;
 import com.ecommerce.services.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Order(1)
 @Component
 @RequiredArgsConstructor
 public class CategorySeeder implements CommandLineRunner {
--- back/src/main/java/com/ecommerce/DataSeeder.java
+++ back/src/main/java/com/ecommerce/DataSeeder.java
@@ -8,6 +8,7 @@ import com.ecommerce.repositories.ProductRepository;
 import com.ecommerce.repositories.SellerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Order(2)
 @Component
 @RequiredArgsConstructor
 public class DataSeeder implements CommandLineRunner {
@@ -71,8 +73,8 @@ public class DataSeeder implements CommandLineRunner {
     Category laptopCategory = categoryRepository.findByCategoryId("electronics_laptops").orElse(null);
     if (laptopCategory != null) {
       allProducts.addAll(Arrays.asList(
-        createProduct("MacBook Air 13” (M2)", "Superleve e potente, com chip M2.", new BigDecimal("10999.00"), new BigDecimal("9899.00"), 8, seller, laptopCategory, Arrays.asList("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-space-gray-select-20220606?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653084303664")),
-        createProduct("MacBook Pro 14” (M3)", "Performance extrema com o novo chip M3 Pro.", new BigDecimal("18499.00"), new BigDecimal("17599.00"), 5, seller, laptopCategory, Arrays.asList("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp14-spaceblack-select-202310?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1697230830200"))
+        createProduct("MacBook Air", "Superleve e potente, com chip M4.", new BigDecimal("10999.00"), new BigDecimal("9899.00"), 8, seller, laptopCategory, List.of("https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/mba13-m4-gallery7-202503?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=TkdkdTYvZmdKUlhJb2I0VXBWbFVFY3VoMUZNNDRVL2pzWmc0SVRMZjBwQXBFd2F2YlBVdmtrWUs2bExmOEpiQ2dsbGZlRHV0Q3JlbHJxWXRlWjZpMnA2UjRCeUZ1cmNrQmUvQ1hmTmlERzA", "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/mba13-m4-midnight-gallery6-202503?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=eE5XY1NucjRjZXpnbnZTeEdWQUdXMDhCb1RMZUtiM1hRQ0dzV0RxT2F4UVdjYmVDUzhhc1RUeW9wRE1pamIxTTczL0VhTmFwUWpXdlYycEJiOVl2QlEyKzhxUFAxVVRPZkcwK3I2eE9sanEyUGd5dG5UTjdmbVJkVGRwbXdDR00", "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/mba13-m4-midnight-gallery1-202503?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=eE5XY1NucjRjZXpnbnZTeEdWQUdXOEN5aTN1Szd3bzB6QjA3QWdrUitrZ1djYmVDUzhhc1RUeW9wRE1pamIxTTczL0VhTmFwUWpXdlYycEJiOVl2QmR6bjJhWkgvT2tCdzFYemk5WGFrSVdDa3lNQlM2VUFiSHRiV1dlZnZaZjQ")),
+        createProduct("MacBook Pro", "Performance extrema com o novo chip M4 Pro.", new BigDecimal("18499.00"), new BigDecimal("17599.00"), 5, seller, laptopCategory, List.of("https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/mbp14-spaceblack-gallery7A-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=YnlWZDdpMFo0bUpJZnBpZjhKM2M3V2Y2dHJYcFh3em9HS2liZTk1WmZvSjdhMHhBVERUd2dsRnlmUnBrSVNSMjczL0VhTmFwUWpXdlYycEJiOVl2QmJXYVJMa1p5Vkc5d2RZOWFRUlVQQTAvUkpMd0VzbTNuK1NFcUNPUjRWM0k", "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/mbp14-spaceblack-gallery1-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=YnlWZDdpMFo0bUpJZnBpZjhKM2M3WGpXSTNqQ2U1MjQxSHBKRkRoWUE0bmd1eUJ6eHZMSFFNMld6aTRncXNRUlJWYlIvRkkxemNIb09FY29ZRmVrUDhQTUF6eWYycDMyY0I5TEVkZkpSbDU4aHA0S1QvclFGZSsvUzZRWUI0U1M", "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/mbp14-spaceblack-gallery6-202410?wid=4000&hei=3074&fmt=jpeg&qlt=90&.v=YnlWZDdpMFo0bUpJZnBpZjhKM2M3ZXB6WmNhSXZMcUE2azZOQ3I1WlA1Zmd1eUJ6eHZMSFFNMld6aTRncXNRUlJWYlIvRkkxemNIb09FY29ZRmVrUDhZRHh1MFdMYTJVMkROQmxjY0dOaVR5U0dRRkxZcDdJYlRIanpUVHlQMzU"))
       ));
     } else {
       System.out.println("AVISO: Categoria 'electronics_laptops' não encontrada. Produtos de MacBook não serão criados.");
@@ -82,8 +84,8 @@ public class DataSeeder implements CommandLineRunner {
     Category tabletCategory = categoryRepository.findByCategoryId("electronics_tablets").orElse(null);
     if (tabletCategory != null) {
       allProducts.addAll(Arrays.asList(
-        createProduct("iPad Pro", "O iPad definitivo com chip M2.", new BigDecimal("8799.00"), new BigDecimal("8199.00"), 12, seller, tabletCategory, Arrays.asList("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-11-select-cell-spacegray-202210?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664411180183")),
-        createProduct("iPad Air", "Colorido e versátil, com o poderoso chip M1.", new BigDecimal("6999.00"), new BigDecimal("6299.00"), 18, seller, tabletCategory, Arrays.asList("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-air-select-wifi-blue-202203?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1645065732688"))
+        createProduct("iPad Pro", "O iPad definitivo com chip M2.", new BigDecimal("8799.00"), new BigDecimal("8199.00"), 12, seller, tabletCategory, List.of("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-11-select-cell-spacegray-202210?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1664411180183")),
+        createProduct("iPad Air", "Colorido e versátil, com o poderoso chip M1.", new BigDecimal("6999.00"), new BigDecimal("6299.00"), 18, seller, tabletCategory, List.of("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-air-select-wifi-blue-202203?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1645065732688"))
       ));
     } else {
       System.out.println("AVISO: Categoria 'electronics_tablets' não encontrada. Produtos de iPad não serão criados.");
@@ -93,8 +95,8 @@ public class DataSeeder implements CommandLineRunner {
     Category wearableCategory = categoryRepository.findByCategoryId("electronics_wearables").orElse(null);
     if (wearableCategory != null) {
       allProducts.addAll(Arrays.asList(
-        createProduct("Apple Watch Series 9", "Mais inteligente, brilhante e poderoso.", new BigDecimal("4999.00"), new BigDecimal("4499.00"), 25, seller, wearableCategory, Arrays.asList("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MTP63ref_VW_34FR+watch-41-alum-midnight-nc-9s_VW_34FR_WF_CO?wid=750&hei=712&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1694507812028")),
-        createProduct("Apple Watch Ultra 2", "Aventura em um novo nível. A caixa de titânio.", new BigDecimal("9699.00"), new BigDecimal("8999.00"), 7, seller, wearableCategory, Arrays.asList("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MT4J3ref_VW_34FR+watch-49-titanium-ultra2_VW_34FR_WF_CO+watch-face-49-alpine-green_VW_34FR_WF_CO?wid=750&hei=712&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1694738596660"))
+        createProduct("Apple Watch Series", "Mais inteligente, brilhante e poderoso.", new BigDecimal("4999.00"), new BigDecimal("4499.00"), 25, seller, wearableCategory, List.of("https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/s10-case-unselect-gallery-1-202503_GEO_BR?wid=5120&hei=3280&fmt=p-jpg&qlt=80&.v=T1poMzZuRzBxQ1RzQmhMUHprUE5LZHlVRllKam5abHNZRGludXlMbytKN0NYcFh4djFrbVFwcjBjSVBjVEoxUWZvSGF2dFhlaXl5ZzZDVTRMdEVvNldmalN5dlRpR2R3QUc1RGJwYllnWml0Tk15R2tiY0pWUEJVRjRRa0RBZXA")),
+        createProduct("Apple Watch Ultra", "Aventura em um novo nível. A caixa de titânio.", new BigDecimal("9699.00"), new BigDecimal("8999.00"), 7, seller, wearableCategory, List.of("https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/ultra-case-unselect-gallery-1-202409_GEO_BR?wid=5120&hei=3280&fmt=p-jpg&qlt=80&.v=aTVJSEliNW9jb25zalBlTm16VmMxcWpkNHRJWDMzcTg3NWRxV0pydTcvSUtncmJvdFd4U3pDVy9GQnFOSXAweUtZMGFKbG9yanhQdjZDS1dZUFFhRWJZc3g4ZkhUVEc0bFNFL1l3MHJZd1Zha1RoZlNOZ09NR2pDSUlzOVhLMmQ"))
       ));
     } else {
       System.out.println("AVISO: Categoria 'electronics_wearables' não encontrada. Produtos de Apple Watch não serão criados.");
